e562b27fba5f6ce6ca43217ca882244705428547
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-26 18:22:06 +0600

    update

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,7 +1,34 @@
 const mongoose= require('mongoose')
 
 const userSchema= new mongoose.Schema({
-
+    name:{
+        type: String, required: true, trim:true
+    },
+    email: {
+        type: String, required: true, trim: true
+    },
+    password:{
+        type: String, required: true, trim: true,
+    },
+    resetToken:{
+        type: String, default: null, trim: true
+    },
+    tokenExpireAt:{
+        type: Date, default: null, trim: true
+    },
+    saved:[
+        {
+            title: {type: String, trim: true},
+            packageId: {type: String, trim: true},
+        }
+    ],
+    order:[
+        {
+            title: {type: String, trim: true},
+            packageId: {type: String, trim: true},
+            payment:{type: String, trim: true},
+        }
+    ]
 })
 
 const User= mongoose.model('users', userSchema)
