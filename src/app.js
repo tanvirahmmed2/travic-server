61995c72eb5a9edcb2af2fc26a56ffb9cc2b5e8b
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-26 18:55:49 +0600

    update

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express= require('express')
+const cors= require('cors')
+const cookieParser= require('cookie-parser')
 const userRouter = require('./router/user.router')
+const tourRouter = require('./router/tour.router')
+const messageRouter = require('./router/message.router')
 
 
 const app= express()
 
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
 
 
 
 app.use('/api/user', userRouter)
+app.use('/api/message', messageRouter)
+app.use('/api/tour', tourRouter)
 
 
 
