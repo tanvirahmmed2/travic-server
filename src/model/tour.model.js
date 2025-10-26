fbdf9b7285ab8bc27bc48d190df74982833c872f
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-26 18:24:01 +0600

    update

--- src/model/tour.model.js
+++ src/model/tour.model.js
@@ -1,7 +1,26 @@
 const mongoose= require('mongoose')
 
 const tourSchema= new mongoose.Schema({
-
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    location:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    price:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    duration:{
+        type: String,
+        required: true,
+        trim: true
+    },
 })
 
 const Tour= mongoose.model('tours', tourSchema)
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,35 +1,38 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 
-const userSchema= new mongoose.Schema({
-    name:{
-        type: String, required: true, trim:true
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true, trim: true
     },
     email: {
         type: String, required: true, trim: true
     },
-    password:{
-        type: String, required: true, trim: true,
-    },
-    resetToken:{
+    phone: {
         type: String, default: null, trim: true
     },
-    tokenExpireAt:{
+    password: {
+        type: String, required: true, trim: true,
+    },
+    resetToken: {
+        type: String, default: null, trim: true
+    },
+    tokenExpireAt: {
         type: Date, default: null, trim: true
     },
-    saved:[
+    saved: [
         {
-            title: {type: String, trim: true},
-            packageId: {type: String, trim: true},
+            title: { type: String, trim: true },
+            packageId: { type: String, trim: true },
         }
     ],
-    order:[
+    order: [
         {
-            title: {type: String, trim: true},
-            packageId: {type: String, trim: true},
-            payment:{type: String, trim: true},
+            title: { type: String, trim: true },
+            packageId: { type: String, trim: true },
+            payment: { type: String, trim: true },
         }
     ]
 })
 
-const User= mongoose.model('users', userSchema)
-module.exports= User
+const User = mongoose.model('users', userSchema)
+module.exports = User
