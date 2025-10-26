445ca5a4dda1cddf3c4fbe3bc05a6163254907f7
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-26 19:06:21 +0600

    update

--- src/controller/message.controller.js
+++ src/controller/message.controller.js
@@ -1,19 +1,20 @@
-const Message = require("../model/message.model")
+const Message = require("../model/message.model");
+const User = require("../model/user.model");
 
-const getMessage=async(req,res)=>{
+const getMessage = async (req, res) => {
     try {
-        const messages= await Message.find({})
-        if(!messages){
-            return res.status(400).send({ 
+        const messages = await Message.find({})
+        if (!messages) {
+            return res.status(400).send({
                 success: false,
-                message: 'No message found' 
+                message: 'No message found'
             });
         }
-        res.status(200).send({ 
-                success: false,
-                message: 'Successfully found messages',
-                payload: messages 
-            });
+        res.status(200).send({
+            success: false,
+            message: 'Successfully found messages',
+            payload: messages
+        });
     } catch (error) {
         res.status(500).send({
             success: false,
@@ -22,6 +23,64 @@ const getMessage=async(req,res)=>{
     }
 }
 
-module.exports={
-    getMessage
+const newMessage = async (req, res) => {
+    try {
+        const { name, email, subject, message } = req.body
+        if (!name || !email || !subject || !message) {
+            return res.status(400).send({
+                success: false,
+                message: 'Fill all field'
+            });
+        }
+        const newMessage = new User({ name, email, subject, message })
+        await newMessage.save()
+        res.status(200).send({
+            success: true,
+            message: `Thank you ${name} fro messaging`
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Operation failed'
+        })
+    }
+}
+
+const removeMessage = async (req, res) => {
+    try {
+        const { id } = req.body
+        if (!id) {
+            return res.status(400).send({
+                success: false,
+                message: 'Message id not found'
+            });
+        }
+        const message = await Message.findById(id)
+        if (!message) {
+            return res.status(400).send({
+                success: false,
+                message: 'Message not found'
+            });
+        }
+        await Message.findByIdAndDelete(id)
+        res.status(200).send({
+            success: false,
+            message: 'Successfully deleted message'
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Failed to remove message'
+        })
+    }
+}
+
+
+
+module.exports = {
+    getMessage,
+    removeMessage,
+    newMessage
 }
