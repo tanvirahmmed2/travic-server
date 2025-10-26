c1cac6a8ceaba1bc6c59f15bcf0ccabe524445b2
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-26 18:25:46 +0600

    update

--- src/model/message.model.js
+++ src/model/message.model.js
@@ -1,7 +1,31 @@
 const mongoose= require('mongoose')
 
 const messageSchema= new mongoose.Schema({
-
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    subject:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    message:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now(),
+        trim: true
+    },
 })
 
 const Message= mongoose.model('messages', messageSchema)
