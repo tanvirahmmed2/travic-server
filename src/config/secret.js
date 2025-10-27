b6159b45e8395e27d88332ac60f9b954ef7b3ae4
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-27 11:17:45 +0600

    update

--- src/config/secret.js
+++ src/config/secret.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 
 const PORT= process.env.PORT
-
+const MONGO_URI= process.env.MONGO_URI
+const CLOUDINARY_NAME=process.env.CLOUDINARY_NAME
+const CLOUDINARY_API_KEY= process.env.CLOUDINARY_API_KEY
+const CLOUDINARY_API_SECRET= process.env.CLOUDINARY_API_SECRET
+const JWT_SECRET= process.env.JWT_SECRET
+const BREVO_API_KEY=process.env.BREVO_API_KEY
+const SENDER_EMAIL=process.env.SENDER_EMAIL
 
 
 
@@ -10,5 +16,12 @@ const PORT= process.env.PORT
 
 module.exports={
     PORT,
-    
+    MONGO_URI,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_NAME,
+    CLOUDINARY_API_SECRET,
+    JWT_SECRET,
+    BREVO_API_KEY,
+    SENDER_EMAIL
+
 }
