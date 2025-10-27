ab60f81ee62036677a615f3c44420896aa8604c7
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-27 11:41:57 +0600

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app= require('./src/app')
+const connectDB = require('./src/config/database')
 const { PORT } = require('./src/config/secret')
 
-
+connectDB()
 
 app.listen(PORT, ()=>{
+    
     console.log(`Server is runnging at http://localhost:${PORT}`)
 })
---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const { MONGO_URI } = require('./secret')
+
+
+const connectDB = async () => {
+    try {
+        if (!MONGO_URI) {
+            console.log(`mongo url missing`)
+        } else {
+            await mongoose.connect(MONGO_URI)
+                .then(() => {
+                    console.log(`Successfully connected mongoDB`)
+                })
+                .catch(() => {
+                    console.log(`Failed to connect mongoDB`)
+                })
+        }
+
+    } catch (error) {
+        console.log(`Server error` + error)
+    }
+}
+
+module.exports = connectDB
